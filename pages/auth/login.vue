66540e613eb0246dcfeefb50a83442eaf03af8c6
xylofonoda <yddrassylo@seznam.cz>
2022-05-09 15:54:59 +0200

    + login page initial

---
+++ pages/auth/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row class="text-center" justify="center">
+    <v-col
+      cols="12"
+      md="3">
+      <div class="pa-7 secondary rounded-circle d-inline-block"></div>
+
+      <div>.rounded-circle</div>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+}
+</script>
