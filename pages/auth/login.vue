4c3012814a52a76ce43188beb6df112f110d0991
xylofonoda <yddrassylo@seznam.cz>
2022-05-09 17:25:53 +0200

    + login page, textfields, light mode

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -5,7 +5,11 @@
       md="3">
       <div class="pa-7 secondary rounded-circle d-inline-block"></div>
 
-      <div>.rounded-circle</div>
+      <div></div>
+      <v-form class="py-">
+        <v-text-field placeholder="ID(E-Mail)" class="py-3"/>
+        <v-text-field placeholder="Password" class="py-3"/>
+      </v-form>
     </v-col>
   </v-row>
 </template>
