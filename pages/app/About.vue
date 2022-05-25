d60ac192f2ffeed95c704a959d229778fea7aa07
xylofonoda <yddrassylo@seznam.cz>
2022-05-25 10:21:52 +0200

    / flexibility of about page

--- pages/app/About.vue
+++ pages/app/About.vue
@@ -1,38 +1,63 @@
 <template>
-  <v-row justify="center">
-    <v-container>
-      <v-expansion-panels accordion class="py-2">
-        <v-expansion-panel
-          v-for="(item,i) in 3"
-          :key="i"
-        >
-          <v-expansion-panel-header class="py-7">
-            <v-col cols="2">
-              <v-icon
-                style="position:absolute; left: 14px; top: 25px"
-                size="30px"
-                color="black"
-              >mdi-card-account-details
-              </v-icon>
-            </v-col>
-            weqweqeq
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo
-            consequat.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-container>
-  </v-row>
+
+  <v-app fluid>
+    <layout-comp title="About"></layout-comp>
+    <v-row justify="center">
+      <v-container>
+        <v-expansion-panels accordion class="py-2">
+          <v-expansion-panel
+            v-for="accord in accords"
+            :key="accord.title"
+          >
+            <v-expansion-panel-header class="py-7">
+              <v-col cols="2">
+                <v-icon
+                  style="position:absolute; left: 14px; top: 25px"
+                  size="30px"
+                  color="black"
+                >mdi-card-account-details
+                </v-icon>
+              </v-col>
+              {{ accord.title }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content
+
+            >
+              <v-col cols="6" class="mx-auto">
+                <p class="text-center">{{accord.desc}}</p>
+              </v-col>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-container>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "AboutPage",
-  layout: "ProjectsLayout"
+  layout: "ProjectsLayout",
+
+
+  data: () => ({
+    miniVariant: false,
+    accords: [
+      {
+        title: 'Contacts',
+        desc:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+      },
+      {
+
+        title: 'Project informations',
+        desc:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+      },
+      {
+
+        title: 'Privacy policy',
+        desc:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+      },
+    ],
+  }),
 }
 </script>
