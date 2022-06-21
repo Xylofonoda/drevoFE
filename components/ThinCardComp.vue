ce161d5103ad75ae4a0c19459a4f3d805136d2f7
xylofonoda <yddrassylo@seznam.cz>
2022-06-21 12:42:23 +0200

    / admindb done / thin card comp resposivity change

--- components/ThinCardComp.vue
+++ components/ThinCardComp.vue
@@ -6,16 +6,16 @@
     rounded
   >
     <v-row>
-      <v-col cols="10">
-        <v-card-title class="text-uppercase pl-5" style="font-size: 15px">
+      <v-col cols="9" xl="10" md="9" sm="9">
+        <v-card-title style="font-weight: 600" class="text-uppercase pl-5 text-md-body-2" >
           {{ title }}
         </v-card-title>
       </v-col>
-      <v-col cols="2">
+      <v-col cols="2" xl="2" md="3">
         <v-btn
           plain
           fab
-          class="mt-3"
+          class="mt-2 ml-xl-3"
           small
         >
           <v-icon>
@@ -24,19 +24,19 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-row no-gutters>
-      <v-col cols="10" xl="10" md="10" sm="10">
+    <v-row >
+      <v-col cols="9" xl="9" md="7" sm="9">
         <v-card-title>
-          <p class="pl-0 text-h4" style="font-weight: 600; ">
+          <p :class="styling" style="font-weight: 600;">
             {{ bignum }}
           </p>
         </v-card-title>
       </v-col>
-      <v-col cols="2" xl="2" md="2" sm="10">
+      <v-col cols="3" xl="3" md="5" sm="3">
         <v-btn
           fab
           x-small
-          class="mt-6 ml-6"
+          class="mt-4 mt-xl-8 mt-md-7 ml-md-15 ml-sm-12"
           plain
         >
           <v-icon color="#F8B400">
@@ -60,6 +60,10 @@ export default {
     bignum: {
       type: String,
       default: null,
+    },
+    styling:{
+      type:String,
+      default:null,
     }
   }
 }
--- pages/admin/admindb.vue
+++ pages/admin/admindb.vue
@@ -1,26 +1,115 @@
 <template>
   <v-app>
     <v-container class="ml-3 pr-10">
-    <v-row>
-      <v-col cols="4">
-        <thin-card-comp
-          title="Registrovaní celkem"
-          bignum="3 675"
-        ></thin-card-comp>
-      </v-col>
-      <v-col cols="4">
-        <thin-card-comp
-          title="uživatelé s min. jednou platbou"
-          bignum="3 675"
-        ></thin-card-comp>
-      </v-col>
-      <v-col cols="4">
-        <thin-card-comp
-          title="Registrovaní celkem"
-          bignum="3 675"
-        ></thin-card-comp>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col
+          v-for="item in cards"
+          :key="item.cardtitle"
+          cols="12"
+          xl="4"
+          md="4"
+          sm="12">
+          <thin-card-comp
+            :title="item.cardtitle"
+            :bignum="item.bignum"
+            :styling="item.styling"
+          ></thin-card-comp>
+        </v-col>
+        <v-col cols="12" xl="4" md="4" sm="12">
+          <v-card
+            class="rounded-xl"
+            outlined
+            color="gray"
+            rounded
+            link
+          >
+            <p class="text-center my-16">
+              <v-btn
+                outlined
+                fab
+                disabled
+
+              >
+                <v-icon
+                  size="50px"
+                  class="mx-auto"
+                >mdi-plus
+                </v-icon>
+              </v-btn>
+            </p>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="pt-xl-5 pt-md-3">
+        <v-col cols="4">
+          <p class="text-start text-uppercase text-xl-h6 mt-xl-3" style="font-weight: 600">
+            registrovaní uživatelé
+          </p>
+        </v-col>
+        <v-col cols="4">
+          <v-card
+            outlined
+            color="gray"
+          >
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="search"
+                  class="px-4 my-0 text-uppercase"
+                  label="hledat"
+                  append-icon="mdi-magnify"
+                  color="gray"
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="4">
+          <v-row class="float-end my-xl-2" align="center">
+            <v-switch
+              color="#F8B400"
+            >
+            </v-switch>
+            <p class="text-xl-subtitle-2 my-0">
+              S platbou
+            </p>
+            <v-btn
+              color="white"
+              small
+              fab
+              plain
+            >
+              <v-icon>
+                mdi-dots-vertical
+              </v-icon>
+            </v-btn>
+          </v-row>
+          <v-col cols="4">
+
+          </v-col>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-data-table
+            :headers="headers"
+            :items="keyinfo"
+            :search="search"
+          ></v-data-table>
+        </v-col>
+      </v-row>
+      <v-btn
+        v-scroll="onScroll"
+        fab
+        fixed
+        bottom
+        right
+        color="#F8B400"
+        @click="toTop"
+      >
+        <v-icon color="black">mdi-chevron-up</v-icon>
+      </v-btn>
     </v-container>
   </v-app>
 </template>
@@ -30,5 +119,165 @@
 export default {
   name: "AdminDatabase",
   layout: "adminlayout",
+
+  data() {
+    return {
+      cards: [
+        {cardtitle: 'Registrovaní celkem', bignum: '3 675', styling: 'text-xl-h3 text-md-h4 text-sm-body-1 '},
+        {cardtitle: 'Uživatelé s min. jednou platbou', bignum: '368', styling: 'text-xl-h3 text-md-h4 text-sm-body-1'},
+        {
+          cardtitle: 'Měření (dostupná/celkem)',
+          bignum: ' 26 759 / 21 674',
+          styling: 'text-xl-h5 text-md-body-2 my-md-4 pb-md-1 text-sm-body-1 '
+        },
+        {
+          cardtitle: 'Projekty (aktivní/celkem)',
+          bignum: '426 / 2 652',
+          styling: 'text-xl-h5 text-md-body-2 my-md-4 pb-md-1 text-sm-body-1 '
+        },
+        {
+          cardtitle: 'Vyfakturováno',
+          bignum: '1 265 000',
+          styling: 'text-xl-h5 text-md-body-2 my-md-4 pb-md-1 text-sm-body-1 '
+        },
+        {cardtitle: 'Hardware', bignum: '12', styling: 'text-xl-h5 text-md-body-2 my-md-4 pb-md-1 text-sm-body-1 '},
+
+      ],
+      search: '',
+      headers: [
+        {
+          text: 'Vytvořeno'.toUpperCase(),
+          align: 'start',
+          sortable: true,
+          value: 'name',
+        },
+        {text: 'Role'.toUpperCase(), value: 'role'},
+        {text: 'Jméno'.toUpperCase(), value: 'jmeno'},
+        {text: 'Firma'.toUpperCase(), value: 'firma'},
+        {text: 'Plán'.toUpperCase(), value: 'plan'},
+        {text: 'Projekty'.toUpperCase(), value: 'projekt'},
+        {text: 'Vyfakturováno'.toUpperCase(), value: 'paidfor'},
+        {text: 'Hardware'.toUpperCase(), value: 'hardware'},
+      ],
+      keyinfo: [
+        {
+          name: '10.6.2023',
+          role: 'admin',
+          jmeno: 'Lorem Ipsum',
+          firma: 'Lorem Ipsum',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Jaroslav Babač',
+          firma: 'Mercury Technologies',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Lorem Ipsum',
+          firma: 'Boar Coms',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ne',
+
+        },
+        {
+          name: '10.6.2023',
+          role: 'admin',
+          jmeno: 'Jiří Chovaneček',
+          firma: 'Cavernetworks',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ne',
+
+        },
+        {
+          name: '10.6.2023',
+          role: 'admin',
+          jmeno: 'Josef Pavlišta',
+          firma: 'Luckyworks',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Ludvík Beskyd',
+          firma: 'Brisco',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+        },
+        {
+          name: '10.6.2023',
+          role: 'admin',
+          jmeno: 'Petr Holub',
+          firma: 'Voyage Navigations',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Jan Vandas',
+          firma: 'Lorem Ipsum',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ne',
+
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Jaroslav Viliš',
+          firma: 'Rush Enterprises',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ne',
+
+        },
+        {
+          name: '10.6.2023',
+          role: 'user',
+          jmeno: 'Martin Mazánek',
+          firma: 'Forestpaw',
+          plan: '36(231)',
+          projekt: '21(31)',
+          paidfor: '26 900',
+          hardware: 'ano',
+        },
+      ],
+    }
+  },
+
+  methods: {
+    onScroll(e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset || e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop() {
+      this.$vuetify.goTo(0)
+    }
+  }
 }
 </script>
